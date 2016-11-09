a360a8b6a9f6c19ba99c879c1caa88ee1bff0e06
Max <themasterlink93@googlemail.com>
2016-11-09 12:46:51 +0100

    script to generate two circles for better testing of ivms

---
+++ RandomForestWithGPs/PythonScripts/generateTwoCircles.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+
+import math
+import random
+import numpy as np
+import operator
+from math import hypot
+import matplotlib.pyplot as plt
+
+
+import os
+import json
+from pprint import pprint
+
+with open("../Settings/init.json") as data_file:
+    data = json.load(data_file)
+
+dim = data["Training"]["dim"]
+
+def generateData(dim, amountOfPoints):
+    text = ""
+    if dim == 0:
+        for i in range(0,amountOfPoints / 2):
+            a = []
+            x = random.uniform(0, 1)
+            y = random.uniform(0, 1)
+            dist = math.cos(2.0 * math.pi * x)*math.sqrt(-2 * math.log(y)) * 0.15
+            angle = random.uniform(0, math.pi * 2)
+            xVal = math.cos(angle) * dist * 2
+            yVal = math.sin(angle) * dist
+            text += str(xVal) + " " + str(yVal) + "\n"
+    else:
+        for i in range(0,amountOfPoints / 2):
+            a = []
+            x = random.uniform(0, 1)
+            y = random.uniform(0, 1)
+            dist = math.cos(2.0 * math.pi * x)*math.sqrt(-2 * math.log(y)) * 0.25
+            angle = random.uniform(0, math.pi * 2)
+            outerAngle = random.uniform(0, math.pi * 2)
+            xVal = math.cos(angle) * dist * 2 + math.cos(outerAngle)  * 2
+            yVal = math.sin(angle) * dist + math.sin(outerAngle) 
+            text += str(xVal) + " " + str(yVal) + "\n"
+    return text
+
+
+train = generateData(dim, int(data["Training"]["amount"]))
+
+ele = 0
+for center in [1,2]:
+    path = data["Training"]["path"] + str(ele) + "/vectors.txt"
+    if not os.path.isdir(data["Training"]["path"] + str(ele) + "/"):
+        os.mkdir(data["Training"]["path"] + str(ele) + "/")
+    if os.path.exists(data["Training"]["path"] + str(ele) + "/vectors.binary"):
+        os.remove(data["Training"]["path"] + str(ele) + "/vectors.binary")
+    f = open(path, "w")
+    f.write(generateData(ele, int(data["Training"]["amount"])))
+    f.close()
+    ele += 1
+
+
+#train += generateData(dim, int(data["Training"]["amount"] / 2))
+
