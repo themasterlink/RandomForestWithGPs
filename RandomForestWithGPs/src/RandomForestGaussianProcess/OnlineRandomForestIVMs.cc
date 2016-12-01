d7f86fe02c1ab65b4da84d0f2fdb4a4cf1e58914
Max <themasterlink93@googlemail.com>
2016-12-01 11:42:11 +0100

    start of ORF with IVM

--- RandomForestWithGPs/src/RandomForestGaussianProcess/OnlineRandomForestIVMs.cc
+++ RandomForestWithGPs/src/RandomForestGaussianProcess/OnlineRandomForestIVMs.cc
@@ -7,16 +7,65 @@
 
 #include "OnlineRandomForestIVMs.h"
 
-OnlineRandomForestIVMs::OnlineRandomForestIVMs() {
-	// TODO Auto-generated constructor stub
+OnlineRandomForestIVMs::OnlineRandomForestIVMs(OnlineStorage<ClassPoint*>& storage, const int maxDepth, const int amountOfUsedClasses):
+	m_storage(storage),
+	m_orf(storage, maxDepth, amountOfUsedClasses),
+	m_amountOfUsedClasses(amountOfUsedClasses),
+	m_firstTrainedDone(false){
 	// removes orf to avoid that the update is called directly to the orf
-//	m_orf.getStorageRef().deattach(&m_orf);
-//	// instead call the update on the
-//	m_orf.getStorageRef().attach(this);
+	m_orf.getStorageRef().deattach(&m_orf);
+	// instead call the update on the
+	m_storage.attach(this);
+
 
 }
 
 OnlineRandomForestIVMs::~OnlineRandomForestIVMs() {
-	// TODO Auto-generated destructor stub
 }
 
+void OnlineRandomForestIVMs::update(Subject* caller, unsigned int event){
+	switch(event){
+	case OnlineStorage<ClassPoint*>::APPEND:{
+		printError("This is not implemented yet!");
+		break;
+	}
+	case OnlineStorage<ClassPoint*>::APPENDBLOCK:{
+		update();
+		break;
+	}
+	case OnlineStorage<ClassPoint*>::ERASE:{
+		printError("This update type is not supported here!");
+		break;
+	}
+	default: {
+		printError("This update type is not supported here!");
+		break;
+	}
+	}
+}
+
+void OnlineRandomForestIVMs::update(){
+	if(!m_firstTrainedDone){
+		m_orf.update(&m_storage, OnlineStorage<ClassPoint*>::APPENDBLOCK);
+	}else{
+		printError("Not implemented yet!");
+	}
+
+}
+
+int OnlineRandomForestIVMs::predict(const DataPoint& point) const{
+	printError("This function is not implemented");
+	return -1;
+}
+
+void OnlineRandomForestIVMs::predictData(const Data& points, Labels& labels) const{
+	printError("This function is not implemented");
+}
+
+void OnlineRandomForestIVMs::predictData(const Data& points, Labels& labels, std::vector< std::vector<double> >& probabilities) const{
+	printError("This function is not implemented");
+}
+
+int OnlineRandomForestIVMs::amountOfClasses() const{
+	return m_amountOfUsedClasses;
+}
--- RandomForestWithGPs/src/RandomForestGaussianProcess/OnlineRandomForestIVMs.h
+++ RandomForestWithGPs/src/RandomForestGaussianProcess/OnlineRandomForestIVMs.h
@@ -8,10 +8,37 @@
 #ifndef RANDOMFORESTGAUSSIANPROCESS_ONLINERANDOMFORESTIVMS_H_
 #define RANDOMFORESTGAUSSIANPROCESS_ONLINERANDOMFORESTIVMS_H_
 
-class OnlineRandomForestIVMs {
+#include "../RandomForests/OnlineRandomForest.h"
+#include "../GaussianProcess/IVMMultiBinary.h"
+#include "../Base/Observer.h"
+
+class OnlineRandomForestIVMs : public PredictorMultiClass, public Observer {
 public:
-	OnlineRandomForestIVMs();
+	OnlineRandomForestIVMs(OnlineStorage<ClassPoint*>& storage, const int maxDepth, const int amountOfUsedClasses);
 	virtual ~OnlineRandomForestIVMs();
+
+	void update(Subject* caller, unsigned int event);
+
+	int predict(const DataPoint& point) const;
+
+	void predictData(const Data& points, Labels& labels) const;
+
+	void predictData(const Data& points, Labels& labels, std::vector< std::vector<double> >& probabilities) const;
+
+	int amountOfClasses() const;
+
+private:
+
+	void update();
+
+	OnlineStorage<ClassPoint*>& m_storage;
+
+	OnlineRandomForest m_orf;
+
+	int m_amountOfUsedClasses;
+
+	bool m_firstTrainedDone;
+
 };
 
 #endif /* RANDOMFORESTGAUSSIANPROCESS_ONLINERANDOMFORESTIVMS_H_ */
